721d41474b55efda5a555216af95acace592f058
smoogipooo <smoogipooo@gmail.com>
2016-03-02 20:48:58 +1030

    Initial.

---
+++ OpenTK Test/GLTest.cs
@@ -0,0 +1,29 @@
+﻿using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.ES20;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenTK_Test
+{
+    class GLTest : GLControl
+    {
+        public GLTest()
+            : base(GraphicsMode.Default, 2, 0, GraphicsContextFlags.Default)
+        {
+            Application.Idle += (o, e) => Invalidate();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.ClearColor(1, 0, 0, 1);
+
+            SwapBuffers();
+        }
+    }
+}
---
+++ OpenTK Test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenTK_Test
+{
+    class Program
+    {
+        public static AutoResetEvent CloseEvent = new AutoResetEvent(false);
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+
+            GLTest test;
+            Application.Run(test = new GLTest());
+        }
+    }
+}
